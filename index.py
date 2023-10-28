181facceaab79d1ccb64103b882ff473a16c7328
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-28 19:21:06 +0100

    commit

---
+++ index.py
@@ -0,0 +1,57 @@
+# myfloat = 7.0
+# print(myfloat)
+# myfloat = float(7)
+# print(myfloat)
+
+# one = 1
+# two = 2
+# three = one + two
+# print(three)
+
+# hello = "hello"
+# world = "world"
+# helloworld = hello + " " + world
+# print(helloworld)
+# change this code
+# mystring = "hello"
+# myfloat = 10.0
+# myint = 20
+
+# # testing code
+# if mystring == "hello":
+#     print("String: %s" % mystring)
+# if isinstance(myfloat, float) and myfloat == 10.0:
+#     print("Float: %f" % myfloat)
+# if isinstance(myint, int) and myint == 20:
+#     print("Integer: %d" % myint)
+# mylist = []
+# mylist.append(4)
+# mylist.append(5)
+# mylist.append(8)
+# print(mylist[0]) # prints 1
+# print(mylist[1]) # prints 2
+# print(mylist[2]) # prints 3
+
+# # prints out 1,2,3
+# for x in mylist:
+#     print(x)
+
+
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("hello")
+strings.append("world")
+
+second_name = names[1]
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
